2ffc1d8cc5fc21453ab0552cf3d5f66f48ee01eb
AbdelkaderA <58740908+AbdelkaderA@users.noreply.github.com>
2020-03-19 19:41:55 +0100

    Modif niveau 1

--- 1-decouvrir-DOM-jquery/niveau1/script.js
+++ 1-decouvrir-DOM-jquery/niveau1/script.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function(){
+    $("#ui").children("button").click(function(listFilm){
+        $("#list").empty();
+        $.get('./playlist.txt', function(donnee){
+            var monString = (donnee.split('\n'));
+            $.each(monString, function(index,value){
+                $("#list").append("<li>"+value+"</li>");                
+            });
+        });
+    });
+});
