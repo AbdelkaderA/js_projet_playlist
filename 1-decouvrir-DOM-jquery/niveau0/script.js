b8aa95774988b2e86de5816c79200a38cbef74d6
AbdelkaderA <58740908+AbdelkaderA@users.noreply.github.com>
2020-03-19 13:45:33 +0100

    Modif niveau 0

--- 1-decouvrir-DOM-jquery/niveau0/script.js
+++ 1-decouvrir-DOM-jquery/niveau0/script.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function message(){
+    
+    $("button").click(function message(){
+        console.log('vous avez réussi !');
+    });    
+    
+});
---
+++ 1-decouvrir-DOM-jquery/niveau0/script2.js
@@ -0,0 +1,7 @@
+$(document).ready(function message(){
+
+    $("#list > span > button").last().click(function () {
+        console.log("vous avez réussi !");
+    });
+
+});
